2b1cce9f0c9f23abf68512092ffce5456419c3e1
Admin <Admin@DESKTOP-RD8GO3G>
2025-03-06 17:50:28 +0100

    Dodaj pliki projektów.

---
+++ NET_lab_0/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace NET_lab_0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            FizzBuzz test = new FizzBuzz(20);
+            test.fizz();
+        }
+    }
+
+    class FizzBuzz
+    {
+        private int zakres;
+        public FizzBuzz(int zakres)
+        {
+            this.zakres = zakres;
+        }
+        public void fizz()
+        {
+            for (int i = 1; i <=zakres; i++)
+            {
+                if (i%3 == 0 && i % 5 != 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                if (i % 5 == 0 && i % 3 != 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                if (i % 3 != 0 && i % 5 != 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
